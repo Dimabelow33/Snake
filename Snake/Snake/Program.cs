6b8816343e143b097d3a0ca84dc5f0669d1963f5
Dimabelow33 <dimabelii205@mail.ru>
2019-01-22 18:48:29 +0300

    Class Snake

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -23,7 +23,8 @@ namespace Snake
             rightline.Draw();
 
             Point p = new Point(20, 20, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
             Console.ReadLine();
         }
     }
